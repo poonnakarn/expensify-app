35e77fb036212fb4c9832b94c3e7140cf1943e6e
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2019-12-06 11:18:27 +0700

    Create private route

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import AppRouter from "./routers/AppRouter";
+import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { login, logout } from "./actions/auth";
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
-import "./firebase/firebase";
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
 
@@ -19,8 +19,31 @@ const jsx = (
   </Provider>
 );
 
+//กรณีอยู่หน้า app อยู่แล้ว ไม่ให้ render ซ้ำ กรณี login หรือ logout
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("app"));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById("app"));
+// check login เมื่อ load app ครั้งแรกหรือมีการ login/logout(AuthStateChange) เท่านั้น
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    // dispatch login() ใน onAuthStateChanged ไม่ใช่ใน startLogin() เพราะว่าต้องการให้ dispatch ทุกครั้งที่มีการ load application
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push("/");
+  }
 });
